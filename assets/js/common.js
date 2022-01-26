a0e98d905e97ed499f56df6c260b2d6a90c79768
ajy <drmoong@gmail.com>
2022-01-26 23:43:37 +0900

    회원가입추가

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,11 +2,21 @@ $(document).ready(function () {
     activeInputBtn();
     deleteInputValue();
     showPassword();
+    proposalEmail();
+    agreeCheckbox();
+    onReadyPopup();
+    onCheckDuplication();
 
+    // test alert
     callAlert({
         title: '테스트용 alert',
         content: '페이지가 열리면 테스트용 alert이 바로 떠버린다',
         btn: '확인',
+    });
+    // test error message
+    onErrorMessage({
+        message: '에러메세지는 input의 "error" 클래스 제거로 초기화',
+        target: '#registId',
     });
 });
 
@@ -64,5 +74,189 @@ function showPassword() {
             $(this).addClass('show');
             $(this).removeClass('hide');
         }
+    });
+}
+
+// 004. 이메일 리스트 추천
+function proposalEmail() {
+    // 입력 또는 포커스일때 이메일 리스트 보이기
+    $(document).on('keyup focus', '#registId', function () {
+        var emailValue = $(this).val();
+        var emailList = $('#emailList');
+        if (
+            emailValue.indexOf('@') > 0 &&
+            emailValue.indexOf('@') + 1 >= emailValue.length
+        ) {
+            for (var i = 0; emailList.length > i; i++) {
+                var item = $(emailList[i]).find('li');
+                item.find('.input_value').html($(this).val());
+            }
+            emailList.css('display', 'block');
+            emailList.animate(
+                {
+                    height: 185,
+                },
+                300
+            );
+        } else {
+            emailList.animate(
+                {
+                    height: 0,
+                },
+                300
+            );
+            setTimeout(function () {
+                emailList.css('display', 'none');
+            }, 300);
+        }
+    });
+
+    // 이메일 리스트 클릭 시 인풋에 적용
+    $(document).on('click', '#emailList li', function () {
+        var target = $('#registId');
+        var targetValue = target.val();
+        var emailList = $('#emailList');
+        target.val(targetValue + $(this).find('.email_address').html());
+
+        emailList.animate(
+            {
+                height: 0,
+            },
+            300
+        );
+        setTimeout(function () {
+            emailList.css('display', 'none');
+        }, 300);
+    });
+
+    // 인풋 포커스 아웃일때 리스트 끄기
+    $(document).on('blur', '#registId', function () {
+        var emailList = $('#emailList');
+        emailList.animate(
+            {
+                height: 0,
+            },
+            300
+        );
+        setTimeout(function () {
+            emailList.css('display', 'none');
+        }, 300);
+    });
+}
+
+// 005. 동의버튼
+function agreeCheckbox() {
+    var agreeList = $('#agreeList');
+    var btnAuthentication = $('#btnAuthentication');
+
+    // 전체동의
+    $(document).on('change', '#agreeBulk', function () {
+        if ($(this).is(':checked')) {
+            agreeList.find('input:checkbox').each(function () {
+                this.checked = true;
+            });
+            btnAuthentication.attr('disabled', false);
+        } else {
+            agreeList.find('input:checkbox').each(function () {
+                this.checked = false;
+            });
+            btnAuthentication.attr('disabled', true);
+        }
+    });
+
+    // 필수동의 인증버튼 활성화
+    $(document).on(
+        'change',
+        '#agreeService, #agreePrivicy, #agreeTotal',
+        function () {
+            checkBtnAuthentication();
+        }
+    );
+}
+
+// 007. 인증버튼 활성화
+function checkBtnAuthentication() {
+    var btnAuthentication = $('#btnAuthentication');
+    if (
+        $('#agreeService').prop('checked') &&
+        $('#agreePrivicy').prop('checked') &&
+        $('#agreeTotal').prop('checked')
+    ) {
+        btnAuthentication.attr('disabled', false);
+    } else {
+        btnAuthentication.attr('disabled', true);
+    }
+}
+
+// 008. 발리데이션 메세지
+function onErrorMessage({ message, target }) {
+    var errorMessage = '<p>' + message + '</p>';
+    var errorBox = $(target).siblings('.error_message');
+
+    errorBox.html('');
+    errorBox.append(errorMessage);
+    $(target).addClass('error');
+
+    // error message는 인풋의 error 클래스 제거로 초기화
+}
+
+// 009. 약관 팝업
+function onReadyPopup() {
+    $(document).on('click', '#agreeList > li > a', function (e) {
+        e.preventDefault();
+        var value = {
+            title: '',
+            value: '',
+            target: '',
+        };
+        var target = $(this).attr('data-popup');
+        switch (target) {
+            case 'agreeService':
+                value.title = '서비스 이용약관';
+                value.text = '약관내용 들어갈자리';
+                value.target = target;
+                break;
+            case 'agreePrivicy':
+                value.title = '개인정보 처리방침';
+                value.text = '약관내용 들어갈자리';
+                value.target = target;
+                break;
+            case 'agreeTotal':
+                value.title = '통합회원가입 약관';
+                value.text = '약관내용 들어갈자리';
+                value.target = target;
+                break;
+            case 'agreeInfo':
+                value.title = '선택정보수집';
+                value.text = '약관내용 들어갈자리';
+                value.target = target;
+                break;
+            case 'agreePrivicy2':
+                value.title = '개인정보처리방침';
+                value.text = '약관내용 들어갈자리';
+                value.target = target;
+                break;
+        }
+        callPopup(value);
+    });
+}
+
+// 010. 본인인증 모달팝업
+function onCheckDuplication() {
+    var modal = $('#authDuplication');
+    modal.css('display', 'flex');
+
+    closeCheckDuplication();
+}
+
+// 011. 본인인증 모달 닫기
+function closeCheckDuplication() {
+    var modal = $('#authDuplication');
+    $(document).on('click', '#authDuplication .result_close', function () {
+        modal.addClass('closing');
+        setTimeout(function () {
+            modal.css('display', 'none');
+            modal.removeClass('closing');
+        }, 300);
     });
 }
--- assets/js/common_ui.js
+++ assets/js/common_ui.js
@@ -1,3 +1,4 @@
+// 알럿
 function callAlert(value) {
     var body = $('body');
     var alert = '';
@@ -10,7 +11,7 @@ function callAlert(value) {
         '</div><div class="btn_group"><button type="button" class="alert_conform">';
     alert += value.btn;
     alert += '</button></div></div><div class="back"></div></div>';
-    body.css('overflow', 'hidden');
+    body.addClass('alert_on');
     body.find('.container').append(alert);
 }
 
@@ -18,5 +19,51 @@ function callAlert(value) {
 $(document).on('click', '.alert_conform, .alert .back', function () {
     var body = $('body');
     $('.alert').remove();
-    body.css('overflow', 'auto');
+    body.removeClass('alert_on');
+});
+
+// 팝업
+function callPopup(value) {
+    var body = $('body');
+    var popup = '';
+    popup +=
+        '<div class="popup"><div class="popup_container"><button type="button" class="popup_close"></button><div class="popup_content">';
+    popup += '<h3 class="title">';
+    popup += value.title;
+    popup += '</h3><pre class="text">';
+    popup += value.text;
+    popup += '</pre></div>';
+    popup += '<button type="button" data-target="';
+    popup += value.target;
+    popup +=
+        '" class="popup_footer_btn">모든 약관에 동의합니다</button></div><div class="back"></div></div>';
+    body.find('.container').append(popup);
+    body.addClass('popup_on');
+}
+
+// 팝업 닫기
+function closePopup() {
+    var body = $('body');
+    var popup = $('.popup');
+    popup.find('.popup_container').addClass('closing');
+
+    setTimeout(function () {
+        popup.remove();
+        body.removeClass('popup_on');
+    }, 300);
+}
+
+// 팝업 닫기 버튼 클릭
+$(document).on('click', '.popup_close, .popup .back', function () {
+    closePopup();
+});
+
+// 팝업 동의
+$(document).on('click', '.popup_footer_btn', function () {
+    var target = $(this).attr('data-target');
+    $('#' + target).prop('checked', true);
+    closePopup();
+
+    // 인증버튼 활성화체크
+    checkBtnAuthentication();
 });
